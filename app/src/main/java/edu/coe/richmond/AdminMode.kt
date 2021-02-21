cd5fdd6b25d6ccf148d361166d8bc1485449ad2b
JP <jeremiah.richmond@midlandu.edu>
2021-02-21 14:34:42 -0600

    Implement second activity, not quite finished

---
+++ app/src/main/java/edu/coe/richmond/AdminMode.kt
@@ -0,0 +1,23 @@
+package edu.coe.richmond
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class AdminMode : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.adminmode)
+        val i = getIntent()
+        val txt = i.extras!!.getString("TEXT")
+        val txtBox = findViewById<TextView>(R.id.IntentTxt)
+        txtBox.text = txt
+
+    }
+
+    fun goBack(v: View) {
+        this.finish()
+    }
+}
+
--- app/src/main/java/edu/coe/richmond/MainActivity.kt
+++ app/src/main/java/edu/coe/richmond/MainActivity.kt
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.content.Intent
+import android.view.View
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.poslayout)
-
-
-
         }
+    fun launchActivity(v: View) {
+        var myIntent = Intent(this, AdminMode::class.java)
+        startActivity(myIntent)
+    }
     }
